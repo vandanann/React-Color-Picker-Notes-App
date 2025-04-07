40a3dfca9eb9c557e499333e83b60f1c538b0d57
vandanann <vandana.nn@yahoo.com>
2025-04-07 14:47:42 +0530

    colorpicker

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import Notes from "./Components/Notes/Notes";
+import Sidenav from "./Components/Sidenav/Sidenav";
+import './App.css'
+export default function App() {
+
+  
+  const items = JSON.parse(localStorage.getItem("notes") || '[]');
+
+  const [addNote, setAddNote] = useState(items);
+  const [favorite, setFavorite] = useState(false)
+  const [delAlert, setDeletAlert] = useState(false)
+
+  const handleAlert = () =>{
+    setDeletAlert(true)
+  }
+  
+  const handleAdd = (color) => {
+    setAddNote([
+      ...addNote,
+      {
+        note: "",
+        color: color,
+        currDate: new Date().toLocaleString('en-GB'),
+        favorite: false,
+        pinned: false
+      }
+    ]);
+  };
+  const handleAddText = (e, index) => {
+    
+    addNote[index].note = e.target.value;
+    localStorage.setItem("notes", JSON.stringify(addNote));
+    
+  };
+  const handleFavorite = (index) =>{
+    if(!addNote[index].favorite){
+      addNote[index].favorite = true
+    }
+    else{
+      addNote[index].favorite = false
+    }
+    setFavorite(!favorite)
+}
+
+  const handlePin = (notes, index) => {
+    if(!addNote.pinned){
+      addNote[index].pinned = true
+      let pinned = addNote.filter((item) => item.notes !== notes);
+      pinned.unshift(addNote[index]);
+      pinned.splice(index + 1, 1);
+      setAddNote(pinned);
+    }
+    else{
+      addNote[index].pinned = false
+    }
+  };
+
+  const handleDelete = (index) => {
+    console.log('ls', items[index])
+    // const deleteItem = items[index]
+    addNote.splice(index, 1);
+    items.splice(index, 1);
+    handleAlert()
+  };
+
+  useEffect(() => {
+    setTimeout(()=>{
+      setDeletAlert(false)
+    },2000)
+    localStorage.setItem("notes", JSON.stringify(addNote));
+  }, [addNote, favorite, delAlert]);
+  return (
+    <div className="App">
+      {
+        delAlert ? <div className="deleteAlert">Note Deleted</div> : null
+      }
+      
+      <Sidenav handleAdd={handleAdd}/>
+      <Notes noteItems={addNote} handleAddText={handleAddText} handleFavorite={handleFavorite} handleDelete={handleDelete} handlePin={handlePin}/>
+    </div>
+  );
+}
---
+++ src/Components/Notes/Notes.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import './notes.css'
+import { AiOutlineStar, AiFillStar, AiOutlineDelete } from 'react-icons/ai';
+import { BsThreeDotsVertical, BsFillPinAngleFill } from 'react-icons/bs';
+
+const Notes = ({ noteItems, handleAddText, handleFavorite, handleDelete, handlePin }) => {
+            
+    const [footerDropDown, setFooterDropDown] = useState()
+    console.log(footerDropDown)
+
+    return (
+        <div className="homeContainer">
+            <div className="mainContainer">
+                <h1 className="heading" style={{fontFamily:"Thasadith"}}>Sticky Notes</h1>
+                <div className="notesContainerInner">
+                    <div className="notes">
+                        {noteItems.map((notes, index) => {
+                            return (
+                                <div className='noteContainer'>
+                                    <div
+                                        className="note"
+                                        key={index}
+                                        style={{ background: `${notes.color}` }}
+                                    >
+                                        <textarea
+                                            className="textBox"
+                                            type="text"
+                                            defaultValue={notes.note}
+                                            onChange={(e) => handleAddText(e, index)}
+                                            placeholder="Write Something"
+                                        />
+                                        <div className="noteFooter">
+                                            <div className='deleteIcon'>
+                                                <BsThreeDotsVertical size={25} onClick={()=>setFooterDropDown(index)} />
+                                                {
+                                                    notes.pinned ? <BsFillPinAngleFill size={25} /> : null
+                                                }
+                                            </div>
+                                            {
+                                                footerDropDown === index ? <div className='footerDropDown' onMouseLeave={()=>setFooterDropDown()}>
+                                                <div className="delete" onClick={()=>{handleDelete(index);setFooterDropDown()}}>
+                                                    <AiOutlineDelete size={25} />
+                                                </div>
+                                                <div className='pinIcon' onClick={()=>handlePin(notes.note, index)}>
+                                                    <BsFillPinAngleFill size={25} />
+                                                </div>
+                                            </div> : null
+                                            }
+                                            <span>{notes.currDate}</span>
+                                            <div className="starIcon" onClick={() => handleFavorite(index)}
+                                                style={{
+                                                    backgroundColor: `${notes.favorite ? 'black' : 'transparent'}`,
+                                                    color: `${!notes.favorite ? 'black' : 'orange'}`
+                                                }}>
+                                                {
+                                                    notes.favorite ? <AiFillStar size={25} /> : <AiOutlineStar size={25} />
+                                                }
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Notes
